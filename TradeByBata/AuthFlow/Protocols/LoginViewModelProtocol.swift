df35c9cdc56fcd2c61974f8901dac2da343e7ae3
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-03-15 14:28:08 +0300

    login protocol

---
+++ TradeByBata/AuthFlow/Protocols/LoginViewModelProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  LoginViewModelProtocol.swift
+//  TradeByBata
+//
+//  Created by Георгий Матченко on 15.03.2023.
+//
+
+protocol LoginViewModelProtocol {
+    func enterFirstName(_ firstname: String)
+    func enterPassword(_ password: String)
+}
