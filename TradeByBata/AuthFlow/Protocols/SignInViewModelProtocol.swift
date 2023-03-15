864e01882c80920ee1d5735664824c140d405268
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-03-15 14:28:41 +0300

    sign in vm protocol

---
+++ TradeByBata/AuthFlow/Protocols/SignInViewModelProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  SignInViewModelProtocol.swift
+//  TradeByBata
+//
+//  Created by Георгий Матченко on 15.03.2023.
+//
+
+protocol SignInViewModelProtocol {
+    func enterFirstName(_ firstname: String)
+    func enterLastName(_ lastname: String)
+    func enterEmail(_ email: String)
+}
