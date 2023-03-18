f41415a752cb51881e085979be4a903760257758
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-03-18 09:31:22 +0300

    latest model

---
+++ TradeByBata/Model/NetwrokLayer/LatestModel.swift
@@ -0,0 +1,13 @@
+//
+//  LatestModel.swift
+//  TradeByBata
+//
+//  Created by Георгий Матченко on 18.03.2023.
+//
+
+import Foundation
+
+struct Latest: Decodable {
+    let latest: [Product]
+}
+
