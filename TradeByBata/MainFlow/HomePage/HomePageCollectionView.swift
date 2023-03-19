57c8a98652fec63a486ab5ba8eb2df19f78e818d
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-03-19 12:19:39 +0300

    init home page module

---
+++ TradeByBata/MainFlow/HomePage/HomePageCollectionView.swift
@@ -0,0 +1,8 @@
+//
+//  HomePageCollectionView.swift
+//  TradeByBata
+//
+//  Created by Георгий Матченко on 19.03.2023.
+//
+
+import Foundation
---
+++ TradeByBata/MainFlow/HomePage/HomePageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomePageViewModel.swift
+//  TradeByBata
+//
+//  Created by Георгий Матченко on 19.03.2023.
+//
+
+import Foundation
