0f4bf5dcf6816f7bc21fbe10e8cc233537856211
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-03-19 12:21:33 +0300

    headers enum

---
+++ TradeByBata/MainFlow/HomePage/Enums/Headers.swift
@@ -0,0 +1,15 @@
+//
+//  Headers.swift
+//  TradeByBata
+//
+//  Created by Георгий Матченко on 19.03.2023.
+//
+
+import Foundation
+
+enum Headers: String {
+    case categories
+    case latest
+    case flashSale
+    case brands
+}
