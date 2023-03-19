545daa5eee0a69245866423b82eecfe195413c97
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-03-19 12:21:09 +0300

    section enum

---
+++ TradeByBata/MainFlow/HomePage/Enums/Section.swift
@@ -0,0 +1,15 @@
+//
+//  Section.swift
+//  TradeByBata
+//
+//  Created by Георгий Матченко on 19.03.2023.
+//
+
+import Foundation
+
+enum Section : Int, CaseIterable {
+    case categories
+    case latest
+    case flashSale
+    case brands
+}
