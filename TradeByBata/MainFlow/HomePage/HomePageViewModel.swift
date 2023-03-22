1bff4f6be5998993588f63d8da45c56d4db8029c
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2023-03-22 19:55:48 +0300

    home page vm updated

--- TradeByBata/MainFlow/HomePage/HomePageViewModel.swift
+++ TradeByBata/MainFlow/HomePage/HomePageViewModel.swift
@@ -14,15 +14,20 @@ class HomePageViewModel: NSObject, HomePageViewModelProtocol {
     var latestProducts: [Product] = []
     var flashSaleProducts: [Product] = []
     
+    override init() {
+        super.init()
+        downloadStartInfo()
+    }
+    
     func checkStatus() {
         if !NetworkManager.shared.loadingFlashSaleStatus && !NetworkManager.shared.loadingLatestStatus {
             loadingStatus = false
         }
     }
     
-    override init() {
-        super.init()
-        downloadStartInfo()
+    func getUserPhoto() -> Data? {
+        guard let userPhoto = LocalStorage.shared.currentUser.photo else { return nil }
+        return userPhoto
     }
     
     func downloadStartInfo() {
@@ -30,8 +35,9 @@ class HomePageViewModel: NSObject, HomePageViewModelProtocol {
         NetworkManager.shared.fetchData(urlString: "https://run.mocky.io/v3/a9ceeb6e-416d-4352-bde6-2203416576ac", model: FlashSale.self) { flashSales in
             NetworkManager.shared.loadingFlashSaleStatus = true
             for item in flashSales.flashSale {
-                var product = Product(category: item.category, name: item.name, price: item.price, discount: item.discount, imageUrl: item.imageUrl, imageData: nil)
-                NetworkManager.shared.loadImage(urlString: item.imageUrl) { imageData in
+                let url = self.changPhotoUrl(name: item.name)
+                var product = Product(category: item.category, name: item.name, price: item.price, discount: item.discount, imageUrl: url, imageData: nil)
+                NetworkManager.shared.loadImage(urlString: url) { imageData in
                     product.imageData = imageData
                     self.flashSaleProducts.append(product)
                     print("\(product.name) is loaded")
@@ -44,8 +50,9 @@ class HomePageViewModel: NSObject, HomePageViewModelProtocol {
         NetworkManager.shared.fetchData(urlString: "https://run.mocky.io/v3/cc0071a1-f06e-48fa-9e90-b1c2a61eaca7", model: Latest.self) { latests in
             NetworkManager.shared.loadingLatestStatus = true
             for item in latests.latest {
-                var product = Product(category: item.category, name: item.name, price: item.price, discount: item.discount, imageUrl: item.imageUrl, imageData: nil)
-                NetworkManager.shared.loadImage(urlString: item.imageUrl) { imageData in
+                let url = self.changPhotoUrl(name: item.name)
+                var product = Product(category: item.category, name: item.name, price: item.price, discount: item.discount, imageUrl: url, imageData: nil)
+                NetworkManager.shared.loadImage(urlString: url) { imageData in
                     product.imageData = imageData
                     self.latestProducts.append(product)
                     print("\(product.name) is loaded")
@@ -53,6 +60,25 @@ class HomePageViewModel: NSObject, HomePageViewModelProtocol {
             }
             NetworkManager.shared.loadingLatestStatus = false
             self.checkStatus()
+        }
+    }
+    
+    private func changPhotoUrl(name: String) -> String{
+        switch name {
+        case "Samsung S10":
+            return "https://i.ebayimg.com/00/s/OTAwWDkwMA==/z/RGAAAOSwgsRivLQ8/$_57.JPG?set_id=8800005007"
+        case "Sony Playstation 5":
+        return "https://avatars.mds.yandex.net/get-marketpic/7074617/pic99bb77dd6fe034aad2d493788d83f1cf/orig"
+        case "Xbox ONE":
+        return "https://kotofoto.ru/product_img/2452/191873/191873_igrovaya_konsol_microsoft_xbox_one_s_1tb_white_3_mesyatsa_game_pass_3_mesyatsa_live_goldm.jpg?v=1549267191"
+        case "BMW X6M":
+        return "https://mobimg.b-cdn.net/v3/fetch/b2/b20fdf022f9587d775298e5c5df3a96f.jpeg"
+        case "New Balance Sneakers":
+            return "https://cdn.dsmcdn.com/mnresize/1200/1800/ty25/product/media/images/20201120/22/29283354/108832210/1/1_org_zoom.jpg"
+        case "Reebok Classic":
+            return "https://cdn1.ozone.ru/s3/multimedia-d/6344448481.jpg"
+        default:
+            return ""
         }
     }
 }
